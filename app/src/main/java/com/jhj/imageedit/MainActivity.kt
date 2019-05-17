0a0cf5fbf4409b8df0ef9bde0651531e9977e9ba
jhj <1406929846@qq.com>
2019-05-17 18:05:39 +0800

    Initial commit

---
+++ app/src/main/java/com/jhj/imageedit/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.jhj.imageedit
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val REQ_IMAGE_EDIT = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val a = "/storage/emulated/0/DCIM/Camera/IMG_20171004_160402.jpg"
+        val b = "/storage/emulated/0/DCIM/Camera/22.jpg"
+        btn_image.setOnClickListener {
+            startActivityForResult(
+                    Intent(this, IMGEditActivity::class.java)
+                            .putExtra(IMGEditActivity.EXTRA_IMAGE_URI, a)
+                            .putExtra(IMGEditActivity.EXTRA_IMAGE_SAVE_PATH, b),
+                    REQ_IMAGE_EDIT
+            )
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQ_IMAGE_EDIT -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    //onImageEditDone()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jhj/imageedit/sticker/IMGSticker.java
@@ -0,0 +1,9 @@
+package com.jhj.imageedit.sticker;
+
+/**
+ * Created by felix on 2017/11/14 下午7:31.
+ */
+
+public interface IMGSticker extends IMGStickerPortrait, IMGViewPortrait {
+
+}
